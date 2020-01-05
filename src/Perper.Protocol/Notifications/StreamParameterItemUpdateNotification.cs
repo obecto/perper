95dc1f0e7d5f14d514ba71aa0b5ff24550201f9e
Branimir Angelov <brani.angelov@gmail.com>
2020-01-05 22:51:43 +0200

    Bugfix: Comma is valid type name symbol

--- src/Perper.Protocol/Notifications/StreamParameterItemUpdateNotification.cs
+++ src/Perper.Protocol/Notifications/StreamParameterItemUpdateNotification.cs
@@ -24,14 +24,14 @@ namespace Perper.Protocol.Notifications
         public override string ToString()
         {
             return
-                $"{nameof(StreamParameterItemUpdateNotification)}<{StreamName},{ParameterName},{ItemStreamName},{ItemType},{ItemKey}>";
+                $"{nameof(StreamParameterItemUpdateNotification)}<{StreamName}%{ParameterName}%{ItemStreamName}%{ItemType}%{ItemKey}>";
         }
 
         public static StreamParameterItemUpdateNotification Parse(string stringValue)
         {
             if (!stringValue.StartsWith(nameof(StreamParameterItemUpdateNotification))) throw new ArgumentException();
 
-            var pattern = $@"{nameof(StreamParameterItemUpdateNotification)}<(?'{nameof(StreamName)}'.*),(?'{nameof(ParameterName)}'.*),(?'{nameof(ItemStreamName)}'.*),(?'{nameof(ItemType)}'.*),(?'{nameof(ItemKey)}'.*)>";
+            var pattern = $@"{nameof(StreamParameterItemUpdateNotification)}<(?'{nameof(StreamName)}'.*)%(?'{nameof(ParameterName)}'.*)%(?'{nameof(ItemStreamName)}'.*)%(?'{nameof(ItemType)}'.*)%(?'{nameof(ItemKey)}'.*)>";
             var match = Regex.Match(stringValue, pattern);
             var streamName = match.Groups[nameof(StreamName)].Value;
             var parameterName = match.Groups[nameof(ParameterName)].Value;
