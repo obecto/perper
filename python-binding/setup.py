a7bdcaa9cb440ce91054e3b674e73b93421a76c4
Branimir Angelov <brani.angelov@gmail.com>
2020-05-15 13:14:47 +0300

    Update version to match C# Functions version

--- python-binding/setup.py
+++ python-binding/setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name='azure-functions-perper-binding',
-    version='0.3.15',
+    version='0.3.16',
     packages=['azure_functions.perper'],
     long_description=long_description,
     long_description_content_type='text/markdown',
