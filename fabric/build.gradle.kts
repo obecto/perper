11d0e6e0baaead12616bf673a4514834c3544076
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2021-04-14 18:07:03 +0300

    chore: Release alpha7

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 }
 
 application {
-    version = "0.6.0-alpha6"
+    version = "0.6.0-alpha7"
     mainClass.set("com.obecto.perper.fabric.Main")
     description = "Perper Fabric"
 }
