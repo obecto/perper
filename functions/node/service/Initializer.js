a8c2f24a18e315d1942ff5080d21dfeadf205e28
Radi Cho <radicho123@gmail.com>
2021-04-18 01:07:24 +0300

    Create temporary Initializer.

--- functions/node/service/Initializer.js
+++ functions/node/service/Initializer.js
@@ -1,10 +1,10 @@
-const IgniteClient = require("apache-ignite-client");
+const IgniteClient = require('apache-ignite-client');
 const IgniteClientConfiguration = IgniteClient.IgniteClientConfiguration;
 
-module.exports = async function(config) {
+module.exports = async function (config) {
   const igniteClient = new IgniteClient();
   await igniteClient.connect(
-    new IgniteClientConfiguration(config.fabric_host + ":10800")
+    new IgniteClientConfiguration(config.fabric_host + ':10800')
   );
 
   return igniteClient;
