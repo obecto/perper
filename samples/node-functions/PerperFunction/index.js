45bdd96b19cdb410b897dbc9439dad485332943c
Radi Cho <radicho123@gmail.com>
2021-04-18 01:08:11 +0300

    Import perper in the azure function.

--- samples/node-functions/PerperFunction/index.js
+++ samples/node-functions/PerperFunction/index.js
@@ -1,3 +1,13 @@
-module.exports = async function (context, perper) {
-    context.log('JavaScript Perper trigger function started with args: '+ JSON.stringify(perper));
+const Initializer = require('perper/service/Initializer');
+const Serializer = require('perper/service/Serializer');
+const PerperInstanceData = require('perper/cache/PerperInstanceData');
+
+module.exports = async function (context, input) {
+  await perper(input);
+};
+
+async function perper (input, types, callback) {
+  const igniteClient = await Initializer({ fabric_host: '127.0.0.1' });
+  const perperInstance = new PerperInstanceData(igniteClient, new Serializer());
+  perperInstance.setTriggerValue(input);
 }
