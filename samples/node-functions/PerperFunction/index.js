327c92529746e43f50dbe19a671de28bb92048c7
Radi Cho <radicho123@gmail.com>
2021-04-18 12:42:36 +0300

    Update azure perper interface.

--- samples/node-functions/PerperFunction/index.js
+++ samples/node-functions/PerperFunction/index.js
@@ -3,11 +3,17 @@ const Serializer = require('perper/service/Serializer');
 const PerperInstanceData = require('perper/cache/PerperInstanceData');
 
 module.exports = async function (context, input) {
-  await perper(input);
+  await perper(input, [String, String, String, Number], async function(a, b, c, d) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+  });
 };
 
-async function perper (input, types, callback) {
+async function perper (input, type, callback) {
   const igniteClient = await initializeIgnite({ fabric_host: '127.0.0.1' });
   const perperInstance = new PerperInstanceData(igniteClient, new Serializer());
-  perperInstance.setTriggerValue(input);
+  const parameters = await perperInstance.setTriggerValue(input, type);
+  console.log(parameters);
 }
