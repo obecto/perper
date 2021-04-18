2ed95f4e855ccf5d21c433d3f5b59e475654bb79
Radi Cho <radicho123@gmail.com>
2021-04-18 12:41:50 +0300

    Update deserialize method.

--- functions/node/service/Serializer.js
+++ functions/node/service/Serializer.js
@@ -4,8 +4,48 @@ Serializer.prototype.serialize = function (data) {
   return data;
 };
 
-Serializer.prototype.deserialize = function (data) {
-  return data;
+Serializer.prototype.deserialize = function (data, type) {
+  const dataType = typeof data;
+  const dataConstructor = data.constructor;
+
+  if (data === null || data === undefined) return null;
+  if (type === null || type === undefined) return data;
+
+  if (['string', 'boolean', 'number'].includes(dataType)) {
+    if (dataConstructor === type) {
+      return data;
+    } else if (type === String) {
+      console.warn('Primitive type conversion applied: Converted ' + dataConstructor.name + ' to String.');
+      return data.toString();
+    } else if (type === Boolean) {
+      let res = (typeof value === 'number' && value > 0) || (typeof value === 'string' && value.toLowerCase() === 'true');
+      console.warn('Primitive type conversion applied: Converted ' + data + ' to ' + res + ' of type Boolean.');
+      return res;
+    } else if (type === Number && !isNaN(parseFloat(data))) {
+      let res = parseFloat(data);
+      console.warn('Primitive type conversion applied: Converted ' + data + ' to ' + res + ' of type Number.');
+      return res;
+    } else {
+      throw new Error('Primitive type mismatch: Cannot convert ' + dataConstructor.name + ' to ' + type + '.');
+    }
+  }
+
+  if (data instanceof Array) {
+    if (type instanceof Array && data.length === type.length) {
+      let res = Array(data.length);
+      for (let i = 0; i < data.length; i++) {
+        res[i] = this.deserialize(data[i], type[i]);
+      }
+
+      return res;
+    } else {
+      throw new Error('Arrays length mismatch: Provided ' + type.length + ' types but got data with length ' + data.length);
+    }
+  };
+
+  console.log('Data:');
+  console.log(data);
+  throw new Error('Could not handle deserialization. Unexpected combination of required types and incoming data.');
 };
 
 module.exports = Serializer;
