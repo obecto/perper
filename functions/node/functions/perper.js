b73f332946dcf271eeffc1030f5f071299dbd427
Radi Cho <radicho123@gmail.com>
2021-04-21 19:43:55 +0300

    Don't apply specific config in the perper node API.

--- functions/node/functions/perper.js
+++ functions/node/functions/perper.js
@@ -2,8 +2,8 @@ const initializeIgnite = require("../utils/initializeIgnite");
 const Serializer = require("../service/Serializer");
 const PerperInstanceData = require("../cache/PerperInstanceData");
 
-module.exports = async function perper(input, type, callback, igniteConfig = {}, mapArrayToParams = true) {
-  const igniteClient = await initializeIgnite(Object.assign({ fabric_host: "127.0.0.1" }, igniteConfig));
+module.exports = async function perper(input, type, callback, mapArrayToParams = true) {
+  const igniteClient = await initializeIgnite({ fabric_host: "127.0.0.1" });
   const perperInstance = new PerperInstanceData(igniteClient, new Serializer());
   const parameters = await perperInstance.setTriggerValue(input, type);
 
