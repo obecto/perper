0a69f5e20bccc4cc13d99a03085e41e12c2d6f8e
Radi Cho <radicho123@gmail.com>
2021-04-25 17:27:18 +0300

    Apply semistandard.

--- samples/node-functions/PerperFunction2/index.js
+++ samples/node-functions/PerperFunction2/index.js
@@ -1,4 +1,4 @@
-module.exports = async function(params) {
-  console.log("PERPER FUNCTION #2");
+module.exports = async function (params) {
+  console.log('PERPER FUNCTION #2');
   console.log(params);
 };
