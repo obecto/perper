3c8229edc4df8ab91e4c55400c189baa075af352
Radi Cho <radicho123@gmail.com>
2021-04-25 14:32:44 +0300

    Test with two functions.

--- samples/data-serialization/ds-perper/Application.cs
+++ samples/data-serialization/ds-perper/Application.cs
@@ -35,8 +35,10 @@ namespace ds_perper
             };
             
             object[] obj = new object[] {"True", false, (double) 5.9, "8.2", 8, dict1, dict1, strs};
+
             // (string, bool, double, string, int) obj = ("True", false, 5.9, "8.2", 8);
-            await context.StartAgentAsync<object>("PerperFunction", obj);
+            var agent = await context.StartAgentAsync<object>("PerperFunction", obj);
+            agent.Item1.CallFunctionAsync<object>("PerperFunction2", obj);
 
             // var (testStream, testStreamName) = await context.CreateBlankStreamAsync<dynamic>();
             // logger.LogInformation("Stream name: {0}", testStreamName);
--- samples/node-functions/index.js
+++ samples/node-functions/index.js
@@ -1,22 +1,30 @@
-const perper = require("perper");
-const perperFunction = require("./PerperFunction/index");
+const perper = require('perper');
+const perperFunction = require('./PerperFunction/index');
 
 const expectedMap = new Map();
 expectedMap.set(0, Boolean);
 expectedMap.set(1, String);
 
+const pTypes = [
+  Boolean,
+  String,
+  String,
+  Number,
+  Number,
+  Map,
+  expectedMap,
+  [String, Boolean, String]
+];
+
 perper({
   PerperFunction: {
-    parameters: [
-      Boolean,
-      String,
-      String,
-      Number,
-      Number,
-      Map,
-      expectedMap,
-      [String, Boolean, String]
-    ],
+    parameters: pTypes,
     action: perperFunction
+  },
+  PerperFunction2: {
+    parameters: pTypes,
+    action: (a) => {
+      console.log("Radi Cho is the best: " + a);
+    }
   }
 });
