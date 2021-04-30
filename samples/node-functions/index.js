a8c2ff9d624f08a215cb1401ff31857e90ec074a
Radi Cho <radicho123@gmail.com>
2021-04-30 12:49:49 +0300

    Update function structure.

--- samples/node-functions/index.js
+++ samples/node-functions/index.js
@@ -4,26 +4,30 @@ const generator = require("./generator/index");
 const processor = require("./processor/index");
 const consumer = require("./consumer/index");
 
-const context = perper({
-  generator: {
-    parameters: [Number],
-    action: generator
-  },
-  processor: {
-    parameters: [Stream, Number],
-    action: processor
-  },
-  consumer: {
-    parameters: [Stream],
-    action: consumer
-  }
-});
+async function main() {
+  const context = await perper({
+    generator: {
+      parameters: [Number],
+      action: generator
+    },
+    processor: {
+      parameters: [Stream, Number],
+      action: processor
+    },
+    consumer: {
+      parameters: [Stream],
+      action: consumer
+    }
+  });
 
-// var generatorStream = context.streamFunction("generator", 20);
-// var processorStream = context.streamFunction(
-//   "processor",
-//   generatorStream,
-//   10
-// );
+  var generatorStream = context.streamFunction("generator", 20);
+  // var processorStream = context.streamFunction(
+  //   "processor",
+  //   generatorStream,
+  //   10
+  // );
 
-// context.streamAction("consumer", processorStream);
+  // context.streamAction("consumer", processorStream);
+}
+
+main();
