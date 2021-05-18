42f996ee700007485beb81694c5229737e0402ac
Radi Cho <radicho123@gmail.com>
2021-05-18 21:21:25 +0300

    Create a perperapi class.

--- samples/python-functions/function/__main__.py
+++ samples/python-functions/function/__main__.py
@@ -1,5 +1,10 @@
 import asyncio
-from perperapi import perperapi
+from perperapi import PerperApi
+
+perperapi = PerperApi()
+context = perperapi.context
+
+generator_stream = context.stream_function('generator', {1:0}, None)
 
 loop = asyncio.get_event_loop()
-result = loop.run_until_complete(perperapi())
+context = loop.run_until_complete(perperapi.handle_notifications())
--- samples/python-functions/function/perperapi.py
+++ samples/python-functions/function/perperapi.py
@@ -1,3 +1,4 @@
+import asyncio
 from pyignite import Client
 
 from perper.cache.perper_instance_data import PerperInstanceData
@@ -8,32 +9,33 @@ from perper.model.state import State
 from perper.model.context import Context
 from perper.services.serializer import Serializer
 from perper.utils.perper_thin_client import PerperThinClient
+from perper.cache.notifications import StreamTriggerNotification
 
 
 # TODO: Move it under functions/python
-async def perperapi():
-    serializer = Serializer()
-    ignite = PerperThinClient()
-    ignite.compact_footer = True
-    ignite.connect('localhost', 10800)
+class PerperApi():
+    def __init__(self):
+        serializer = Serializer()
+        self.ignite = PerperThinClient()
+        self.ignite.compact_footer = True
+        self.ignite.connect('localhost', 10800)
 
-    config = PerperConfig()
-    fs = FabricService(ignite, config)
-    fs.start()
+        config = PerperConfig()
+        self.fs = FabricService(self.ignite, config)
+        self.fs.start()
 
-    instance = PerperInstanceData(ignite, serializer)
-    state = State(instance, ignite, serializer)
-    context = Context(instance, fs, state, ignite)
+        self.instance = PerperInstanceData(self.ignite, serializer)
+        self.state = State(self.instance, self.ignite, serializer)
+        self.context = Context(self.instance, self.fs, self.state, self.ignite)
 
-    async for (k, n) in fs.get_notifications():
-        print(n)
-    
-        # if isinstance(n, StreamItemNotification):
-        #     cache = ignite.get_cache(n.cache)
-        #     item = cache.get(n.key)
-        #     print(f"Item received from [{n.cache}]: " + str(item))
-        #     fs.consume_notification(k)
-        pass
 
-    return context
+    async def handle_notifications(self): 
+        async for (k, n) in self.fs.get_notifications():
+            print(n)
 
+            # if isinstance(n, StreamTriggerNotification):
+            #     cache = self.ignite.get_cache(n.cache)
+            #     item = cache.get(n.key)
+            #     print(f"Item received from [{n.cache}]: " + str(item))
+            #     self.fs.consume_notification(k)
+            pass
