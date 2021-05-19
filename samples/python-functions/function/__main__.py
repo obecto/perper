9277a22e87dd641ffe91b65073eebae2636d49d2
Radi Cho <radicho123@gmail.com>
2021-05-19 21:35:30 +0300

    Create simple agent. Generator, processor, consumer.

--- samples/python-functions/function/__main__.py
+++ samples/python-functions/function/__main__.py
@@ -1,16 +1,30 @@
 import asyncio
-from perperapi import PerperApi
+from perperapi import Perper
 
 def generator(perperapi_instance, *kwargs):
-    print(kwargs)
     print("GENERATOR!")
 
-functions = {"generator": generator}
+def processor(perperapi_instance, *kwargs):
+    print("PROCESSOR!")
 
-perperapi = PerperApi()
-context = perperapi.context
+def consumer(perperapi_instance, *kwargs):
+    print("CONSUMER!")
+    streams_cache = perperapi_instance.ignite.get_cache('streams')
+    print("Consumed:")
+    print(streams_cache.get_and_remove(kwargs[1][0].streamname))
 
-generator_stream = context.stream_function("generator", {1:0}, None)
+functions = {
+    "generator": generator,
+    "processor": processor,
+    "consumer": consumer
+}
+
+perper = Perper()
+context = perper.context
+
+generator_stream = context.stream_function("generator", {0: 20}, None)
+processor_stream = context.stream_function("processor", {0: 21, 1: generator_stream}, None)
+consumer_stream = context.stream_action("consumer", {0: processor_stream}, None)
 
 loop = asyncio.get_event_loop()
-context = loop.run_until_complete(perperapi.handle_notifications(functions))
+context = loop.run_until_complete(perper.functions(functions))
