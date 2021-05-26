3512ebb04ae511a2e1a31f947e49634afecd6bd5
Nikolay Dermendzhiev <dermendzhiev.n@gmail.com>
2021-05-26 19:09:58 +0300

    refactor: suppress rule about Delegate reserved name

--- functions/runtime/src/Perper.WebJobs.Extensions/Cache/IInstanceData.cs
+++ functions/runtime/src/Perper.WebJobs.Extensions/Cache/IInstanceData.cs
@@ -3,8 +3,12 @@ namespace Perper.WebJobs.Extensions.Cache
     public interface IInstanceData
     {
         string Agent { get; set; }
+
         string AgentDelegate { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1716:Identifiers should not match keywords", Justification = "<Pending>")]
         string Delegate { get; set; }
+
         object? Parameters { get; set; }
     }
 }
