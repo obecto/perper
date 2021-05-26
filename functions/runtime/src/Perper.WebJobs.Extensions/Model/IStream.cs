a7c70e0fe1b45ab85cdec9a176754cfd4087d1a3
Nikolay Dermendzhiev <dermendzhiev.n@gmail.com>
2021-05-26 19:05:37 +0300

    refactor: add suppression attributes for Stream types

--- functions/runtime/src/Perper.WebJobs.Extensions/Model/IStream.cs
+++ functions/runtime/src/Perper.WebJobs.Extensions/Model/IStream.cs
@@ -5,6 +5,7 @@ using System.Linq.Expressions;
 
 namespace Perper.WebJobs.Extensions.Model
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "<Pending>")]
     public interface IStream<T> : IAsyncEnumerable<T>, IStream
     {
         IAsyncEnumerable<T> DataLocal();
@@ -14,6 +15,7 @@ namespace Perper.WebJobs.Extensions.Model
         IAsyncEnumerable<TResult> Query<TResult>(Func<IQueryable<T>, IQueryable<TResult>> query);
     }
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "<Pending>")]
     public interface IStream
     {
     }
