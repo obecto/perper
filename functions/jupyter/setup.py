dd1a0927350b3459ad62e44476bea74eb2f1caa3
Radi Cho <radicho123@gmail.com>
2021-05-29 17:34:44 +0300

    Separate Jupyter package.

---
+++ functions/jupyter/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='ggbg',
+    version='0.5.1',
+    packages=['perper_jupyter'],
+    install_requires=['perper', 'ipython', 'jupyterlab'],
+)
