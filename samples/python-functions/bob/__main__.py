823d4d41395c7a07f1cc514fef04d4de476255e7
Radi Cho <radicho123@gmail.com>
2021-05-30 22:34:17 +0300

    Test Alice-Bob with a simple BoolResult wrapper.

--- samples/python-functions/bob/__main__.py
+++ samples/python-functions/bob/__main__.py
@@ -4,6 +4,15 @@ import asyncio
 from perper.functions import Perper
 from perper.cache.stream_data import ParameterData
 
+from collections import OrderedDict
+from pyignite import GenericObjectMeta
+from pyignite.datatypes import BoolObject
+
+class BoolResult(metaclass=GenericObjectMeta, schema=OrderedDict([
+    ('value', BoolObject)
+])):
+    pass
+
 os.environ["PERPER_AGENT_NAME"] = "Bob"
 
 perper = Perper()
@@ -11,6 +20,6 @@ context = perper.context
 
 async def Bob(perper_instance, *kwargs):
     print(kwargs)
-    return True
+    return BoolResult(value=True)
 
 asyncio.run(perper.functions({'Bob': Bob}))
