0f1cb04ebd05b1062fa52b67a992bf0550552b9c
Radi Cho <radicho123@gmail.com>
2021-05-30 12:22:52 +0300

    Initialize Bob.

--- samples/python-functions/alice/__main__.py
+++ samples/python-functions/alice/__main__.py
@@ -4,15 +4,15 @@ import asyncio
 from perper.functions import Perper
 from perper.cache.stream_data import ParameterData
 
-perper = Perper()
-context = perper.context
-
 os.environ["PERPER_AGENT_NAME"] = "Alice"
 os.environ["PERPER_ROOT_AGENT"] = "Alice"
 
+perper = Perper()
+context = perper.context
+
 async def launcher():
-    agent_name = 'Bob'
-    caller_agent_name_parameter = 'Alice'
+    agent_name = "Bob"
+    caller_agent_name_parameter = "Alice"
 
     (agent, result) = await context.start_agent(agent_name, {0: caller_agent_name_parameter})
     print(result)
--- samples/python-functions/bob/__main__.py
+++ samples/python-functions/bob/__main__.py
@@ -4,17 +4,13 @@ import asyncio
 from perper.functions import Perper
 from perper.cache.stream_data import ParameterData
 
+os.environ["PERPER_AGENT_NAME"] = "Bob"
+
 perper = Perper()
 context = perper.context
 
-os.environ["PERPER_AGENT_NAME"] = "Alice"
-os.environ["PERPER_ROOT_AGENT"] = "Alice"
+async def Bob(perper_instance, *kwargs):
+    print(kwargs)
+    return True
 
-async def launcher():
-    agent_name = 'Bob'
-    caller_agent_name_parameter = 'Alice'
-
-    (agent, result) = await context.start_agent(agent_name, {0: caller_agent_name_parameter})
-    print(result)
-
-asyncio.run(launcher())
+asyncio.run(perper.functions({'Bob': Bob}))
