660e40152176d51128d9c5627de8799332ab4f65
Radi Cho <radicho123@gmail.com>
2021-05-31 13:27:12 +0300

    Drop BoolResult class passing.

--- samples/python-functions/bob/__main__.py
+++ samples/python-functions/bob/__main__.py
@@ -4,22 +4,14 @@ import asyncio
 from perper.functions import Perper
 from perper.cache.stream_data import ParameterData
 
-from collections import OrderedDict
-from pyignite import GenericObjectMeta
-from pyignite.datatypes import BoolObject
-
-class BoolResult(metaclass=GenericObjectMeta, schema=OrderedDict([
-    ('value', BoolObject)
-])):
-    pass
-
 os.environ["PERPER_AGENT_NAME"] = "Bob"
 
 perper = Perper()
 context = perper.context
 
 async def Bob(perper_instance, *kwargs):
-    print(kwargs)
-    return BoolResult(value=True)
+    stream = context.create_blank_stream(basename='generator')
+    print(stream)
+    return stream
 
 asyncio.run(perper.listen_triggers({'Bob': Bob}))
