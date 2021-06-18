e93f972deb78611bf27fc202a702f45bfc26fea5
Radi Cho <radicho123@gmail.com>
2021-06-18 13:00:34 +0300

    Tests.

--- agent/python/perper/test.py
+++ agent/python/perper/test.py
@@ -11,17 +11,17 @@ ignite = PerperIgniteClient()
 with ignite.connect('127.0.0.1', 10800):
     cache_service = CacheService(ignite)
 
-    # print('Stream tests:')
-    # STREAM_NAME = 'test_stream'
-    # cache_service.stream_create(STREAM_NAME, 'test_instance', 'test_agent', 'test_bool_stream_delegate', 1, True, BoolObject, ephemeral = False)
-    # key = cache_service.stream_write_item(STREAM_NAME, 'Hello world')
-    # item = cache_service.stream_read_item(STREAM_NAME, key)
-    # print(key, item)
+    print('Stream tests:')
+    STREAM_NAME = 'test_stream'
+    cache_service.stream_create(STREAM_NAME, 'test_instance', 'test_agent', 'test_bool_stream_delegate', 1, True, BoolObject, ephemeral = False)
+    key = cache_service.stream_write_item(STREAM_NAME, 'Hello world')
+    item = cache_service.stream_read_item(STREAM_NAME, key)
+    print(key, item)
 
-    # listener = cache_service.stream_add_listener(STREAM_NAME, 'caller_agent', 'caller', 1)
-    # print(listener)
+    listener = cache_service.stream_add_listener(STREAM_NAME, 'caller_agent', 'caller', 1)
+    print(listener)
 
-    # cache_service.stream_remove_listener(STREAM_NAME, listener)
+    cache_service.stream_remove_listener(STREAM_NAME, listener)
 
     print('Call tests:')
     cache_service.call_create('test_call1', 'test_instance', 'test_agent', 'test_bool_stream_delegate', 'caller_agent', 'caller', True, BoolObject)
