d8afa63a8dc7e10879cc93af59662fb386c8439a
Radi Cho <radicho123@gmail.com>
2021-06-18 14:43:09 +0300

    Polish cache service up.

--- agent/python/perper/cache_service.py
+++ agent/python/perper/cache_service.py
@@ -1,8 +1,9 @@
 import uuid
 from datetime import datetime
 
-from stream_data import create_stream_data, create_stream_listener, stream_data_add_listener, stream_data_remove_listener
-from call_data import create_call_data, set_call_data_result, set_call_data_error, set_call_data_finished
+from stream_data import *
+from call_data import *
+from ignite_cache_extensions import put_if_absent_or_raise, optimistic_update
 
 class CacheService:
     def __init__(self, ignite):
@@ -22,22 +23,22 @@ class CacheService:
 
     def stream_create(self, stream, instance, agent, delegate, delegate_type, parameters, parameters_type, ephemeral = True):
         stream_data = create_stream_data(instance, agent, delegate, delegate_type, ephemeral, parameters, parameters_type)
-        return self.ignite.put_if_absent_or_raise(self.streams_cache, stream, stream_data)
+        return put_if_absent_or_raise(self.streams_cache, stream, stream_data)
 
 
     def stream_add_listener(self, stream, caller_agent, caller, parameter, filter = {}, replay = False, local_to_data = False):
         stream_listener = create_stream_listener(caller_agent, caller, parameter, replay, local_to_data=local_to_data, filter=filter)
 
-        self.ignite.optimistic_update(self.streams_cache, stream, lambda data: stream_data_add_listener(data, stream_listener))
+        optimistic_update(self.streams_cache, stream, lambda data: stream_data_add_listener(data, stream_listener))
         return stream_listener
 
     def stream_remove_listener(self, stream, stream_listener): # TODO: Implement StreamRemoveListener(string stream, string caller, int parameter)
-        return self.ignite.optimistic_update(self.streams_cache, stream, lambda data: stream_data_remove_listener(data, stream_listener))
+        return optimistic_update(self.streams_cache, stream, lambda data: stream_data_remove_listener(data, stream_listener))
 
     def stream_write_item(self, stream, item):
         items_cache = self.ignite.get_or_create_cache(stream)
         key = self.get_current_ticks()
-        self.ignite.put_if_absent_or_raise(items_cache, key, item)
+        put_if_absent_or_raise(items_cache, key, item)
         return key
 
     def stream_read_item(self, cache, key):
@@ -48,16 +49,16 @@ class CacheService:
 
     def call_create(self, call, instance, agent, delegate, caller_agent, caller, parameters, parameters_type, local_to_data=False,):
         call_data = create_call_data(instance, agent, delegate, caller_agent, caller, local_to_data, parameters, parameters_type)
-        return self.ignite.put_if_absent_or_raise(self.calls_cache, call, call_data)
+        return put_if_absent_or_raise(self.calls_cache, call, call_data)
 
     def call_write_result(self, call, result, result_type):
-        return self.ignite.optimistic_update(self.calls_cache, call, lambda data: set_call_data_result(data, result, result_type))
+        return optimistic_update(self.calls_cache, call, lambda data: set_call_data_result(data, result, result_type))
 
     def call_write_error(self, call, error):
-        return self.ignite.optimistic_update(self.calls_cache, call, lambda data: set_call_data_error(data, error))
+        return optimistic_update(self.calls_cache, call, lambda data: set_call_data_error(data, error))
 
     def call_write_finished(self, call):
-        return self.ignite.optimistic_update(self.calls_cache, call, set_call_data_finished)
+        return optimistic_update(self.calls_cache, call, set_call_data_finished)
 
     def call_read_error(self, call):
         call_data = self.calls_cache.get(call)
--- agent/python/perper/thin_client.py
+++ agent/python/perper/thin_client.py
@@ -8,15 +8,3 @@ class PerperIgniteClient(Client):
     def compact_footer(self, val):
         pass
 
-    def optimistic_update(self, cache, key, update_func):
-        while True:
-            existing_value = cache.get(key)
-            new_value = update_func(existing_value)
-            if (cache.replace_if_equals(key, existing_value, new_value)):
-                break
-
-    def put_if_absent_or_raise(self, cache, key, value):
-        result = cache.put_if_absent(key, value)
-        if result is None:
-            raise Exception('Duplicate cache item key! (key is {key})')
-
