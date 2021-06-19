af8f599054e58f3c967aae4d820bee5b868cc86a
Radi Cho <radicho123@gmail.com>
2021-06-19 18:02:46 +0300

    Fix and test get_call_result_notification.

--- agent/python/perper/notification_service.py
+++ agent/python/perper/notification_service.py
@@ -1,45 +1,17 @@
 import fabric_pb2, fabric_pb2_grpc
-
-from collections import OrderedDict
-from pyignite import GenericObjectMeta
-from pyignite.datatypes import LongObject, String
+from notifications.notification_key_long import NotificationKeyLong
+from notifications.notification_key_string import NotificationKeyString
 
 class NotificationService:
     def __init__(self, ignite, grpc_channel, agent):
         self.agent = agent
-        self.notifications_cache = ignite.get_or_create_cache('{agent}-$notifications')
+        self.notifications_cache = ignite.get_or_create_cache(f'{agent}-$notifications')
         self._grpc_channel = grpc_channel
 
     def consume_notification(self, key):
         return self.notifications_cache.remove(key)
 
     def get_notification_key(self, notification: fabric_pb2.Notification): # TODO: Implement properly
-
-        class NotificationKeyString(
-            metaclass=GenericObjectMeta,
-            type_name='NotificationKeyString',
-            schema=OrderedDict(
-                [
-                    ('affinity', String),
-                    ('key', LongObject),
-                ]
-            ),
-        ):
-            pass
-
-
-        class NotificationKeyLong(
-            metaclass=GenericObjectMeta,
-            type_name='NotificationKeyLong',
-            schema=OrderedDict(
-                [
-                    ('affinity', LongObject),
-                    ('key', LongObject),
-                ]
-            ),
-        ):
-            pass
-
         if notification.stringAffinity not in (None, ''):
             return NotificationKeyString(
                 key=notification.notificationKey, affinity=notification.stringAffinity
@@ -56,8 +28,8 @@ class NotificationService:
         grpc_stub = fabric_pb2_grpc.FabricStub(self._grpc_channel)
         notification = grpc_stub.CallResultNotification(
             fabric_pb2.CallNotificationFilter(
-                Agent = self.agent,
-                Call = call
+                agent = self.agent,
+                call = call
             )
         )
 
---
+++ agent/python/perper/test.py
@@ -0,0 +1,51 @@
+from pyignite import Client, GenericObjectMeta
+from collections import OrderedDict
+from pyignite.datatypes import String, BoolObject
+
+from thin_client import PerperIgniteClient
+from call_data import create_call_data, set_call_data_result
+from cache_service import CacheService
+from stream_data import *
+
+import grpc
+from notification_service import NotificationService
+
+import asyncio
+
+ignite = PerperIgniteClient()
+async def test():
+    with ignite.connect('127.0.0.1', 10800):
+        cache_service = CacheService(ignite)
+        channel = grpc.insecure_channel('127.0.0.1:40400')
+        notification_service = NotificationService(ignite, channel, 'caller_agent')
+
+        # print('Stream cache tests:')
+        # STREAM_NAME = 'test_stream'
+        # cache_service.stream_create(STREAM_NAME, 'test_instance', 'test_agent', 'test_bool_stream_delegate', 1, True, BoolObject, ephemeral = False)
+        # key = cache_service.stream_write_item(STREAM_NAME, 'Hello world')
+        # item = cache_service.stream_read_item(STREAM_NAME, key)
+        # print(key, item)
+
+        # listener = cache_service.stream_add_listener(STREAM_NAME, 'caller_agent', 'caller', 1)
+        # print(listener)
+
+        # cache_service.stream_remove_listener(STREAM_NAME, listener)
+
+        # print('Call cache tests:')
+        # cache_service.call_create('test_call1', 'test_instance', 'test_agent', 'test_bool_stream_delegate', 'caller_agent', 'caller', True, BoolObject)
+        cache_service.call_create('test_call2', 'test_instance', 'test_agent', 'test_bool_stream_delegate', 'caller_agent', 'caller', True, BoolObject)
+        
+        # cache_service.call_write_error('test_call1', 'Radi said it is an error!')
+        # error = cache_service.call_read_error('test_call1')
+        # print(error)
+
+        cache_service.call_write_result('test_call2', 'Result here!', String)
+        # error, result = cache_service.call_read_error_and_result('test_call2')
+        # print(error, result)
+        
+        print('Notification Service tests:')
+        k, i = await notification_service.get_call_result_notification('test_call2')
+        print(k, i)
+
+if __name__ == "__main__":
+    asyncio.run(test())
