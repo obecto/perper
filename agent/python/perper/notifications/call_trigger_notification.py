8a2de9dde3a8d71eb2ff8f90d35a8a94751a666c
Radi Cho <radicho123@gmail.com>
2021-06-19 19:40:10 +0300

    Add threading.

--- agent/python/perper/notifications/call_trigger_notification.py
+++ agent/python/perper/notifications/call_trigger_notification.py
@@ -5,10 +5,11 @@ from pyignite.datatypes import String
 
 class CallTriggerNotification(
     metaclass=GenericObjectMeta,
+    type_name='CallTriggerNotification',
     schema=OrderedDict(
         [
-            ("call", String),
-            ("delegate", String),
+            ('call', String),
+            ('delegate', String),
         ]
     ),
 ):
