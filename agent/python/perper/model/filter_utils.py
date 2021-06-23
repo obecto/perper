9154dffb9abc132d2dce7d27d3689a8f57a2938e
Radi Cho <radicho123@gmail.com>
2021-06-23 18:37:50 +0300

    Initialize filter utils.

---
+++ agent/python/perper/model/filter_utils.py
@@ -0,0 +1,16 @@
+class FilterUtils:
+    @staticmethod
+    def parse_field_name(subexpression):
+        pass
+
+    @staticmethod
+    def parse_field_value(subexpression):
+        pass
+
+    @staticmethod
+    def add_to_filter(_filter, subexpression):
+        pass
+
+    @classmethod
+    def convert_filter(cls, _filter):
+        return _filter
