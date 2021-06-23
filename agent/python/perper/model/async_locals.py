247e7642b139e9876c4d84d5a349bdb8ca611246
Radi Cho <radicho123@gmail.com>
2021-06-23 18:37:20 +0300

    Update async locals.

--- agent/python/perper/model/async_locals.py
+++ agent/python/perper/model/async_locals.py
@@ -1,17 +1,25 @@
 from contextvars import ContextVar
 
-class AsyncLocals:
-    connection = ContextVar('connection')
-    instance = ContextVar('instance')
+connection = ContextVar('connection')
+instance = ContextVar('instance')
 
+class AsyncLocals:
     @staticmethod
     def set_connection(cache_service, notification_service):
-        AsyncLocals.connection.set((cache_service, notification_service))
+        connection.set((cache_service, notification_service))
     
     @staticmethod
     def get_cache_service():
-        return AsyncLocals.connection.get()[0]
+        return connection.get()[0]
 
     @staticmethod
     def get_notification_service():
-        return AsyncLocals.connection.get()[1]
+        return connection.get()[1]
+
+    @staticmethod
+    def get_agent():
+        return get_notification_service().agent
+
+    @staticmethod
+    def get_instance():
+        return instance.get()
