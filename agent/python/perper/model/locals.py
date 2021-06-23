f87f25317fd8a0ebeda314154625c92a03b1bfa8
Radi Cho <radicho123@gmail.com>
2021-06-23 09:37:35 +0300

    Context vars.

--- agent/python/perper/model/locals.py
+++
@@ -1,8 +0,0 @@
-class Locals:
-    cache_service = None
-    notification_service = None
-
-    @staticmethod
-    def set_connection(cache_service, notification_service):
-        Locals.cache_service = cache_service
-        Locals.notification_service = notification_service
