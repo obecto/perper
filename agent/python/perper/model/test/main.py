66888deaa63037657caefda0d8adeec105999691
Radi Cho <radicho123@gmail.com>
2021-06-25 23:23:08 +0300

    Update AsyncLocals implementation.

--- agent/python/perper/model/test/main.py
+++ agent/python/perper/model/test/main.py
@@ -7,17 +7,17 @@ from perper.protocol.cache_service import CacheService
 from perper.protocol.notification_service import NotificationService
 from perper.protocol.standard import PerperStream
 from perper.model.stream import Stream
-from perper.model.async_locals import AsyncLocals
+from perper.model.async_locals import *
 
 ignite = PerperIgniteClient()
 def test():
     with ignite.connect('127.0.0.1', 10800):
         cache_service = CacheService(ignite)
         notification_service = NotificationService(ignite, '127.0.0.1:40400', 'test_agent')
-        AsyncLocals.set_connection(cache_service, notification_service)
+        set_connection(cache_service, notification_service)
 
-        print(AsyncLocals.get_cache_service())
-        print(AsyncLocals.get_notification_service())
+        print(get_cache_service())
+        print(get_notification_service())
 
         notification_service.start()
         cache_service.stream_create('test_stream', 'test_instance', 'test_agent', 'test_delegate', 2, True, BoolObject)
@@ -28,6 +28,6 @@ def test():
             async for value in generator:
                 print(value)
 
-        AsyncLocals.enter_context('test_instance',  lambda: asyncio.run(listen_items()))
+        enter_context('test_instance',  lambda: asyncio.run(listen_items()))
 
 test()
