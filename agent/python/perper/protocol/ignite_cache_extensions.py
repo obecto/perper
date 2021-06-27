15e32954c109a0ad68bdc7f97644c60e35c7dcee
Radi Cho <radicho123@gmail.com>
2021-06-27 21:28:33 +0300

    Workaround optimistic update.

--- agent/python/perper/protocol/ignite_cache_extensions.py
+++ agent/python/perper/protocol/ignite_cache_extensions.py
@@ -1,9 +1,14 @@
 def optimistic_update(cache, key, update_func):
-    while True:
-        existing_value = cache.get(key)
-        new_value = update_func(existing_value)
-        if (cache.replace_if_equals(key, existing_value, new_value)):
-            break
+    existing_value = cache.get(key)
+    new_value = update_func(existing_value)
+    cache.put(key, new_value)
+
+    # TODO: FIX
+    # while True:
+    #     existing_value = cache.get(key)
+    #     new_value = update_func(existing_value)
+    #     if cache.replace_if_equals(key, existing_value, new_value):
+    #         break
 
 def put_if_absent_or_raise(cache, key, value):
     result = cache.put_if_absent(key, value)
