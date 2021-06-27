16de1e4d8fe24ac316dccde2bf15e4d937a71297
Radi Cho <radicho123@gmail.com>
2021-06-27 15:17:33 +0300

    Try using the sample code directly inside of jupyter.

--- agent/python/perper/model/test/main.py
+++ agent/python/perper/model/test/main.py
@@ -8,25 +8,27 @@ from perper.protocol.notification_service import NotificationService
 from perper.protocol.standard import PerperStream
 from perper.model.stream import Stream
 from perper.model.async_locals import *
+from perper.model.context import *
 
 ignite = Client()
-def test():
+async def test():
     with ignite.connect('127.0.0.1', 10800):
         cache_service = CacheService(ignite)
-        notification_service = NotificationService(ignite, '127.0.0.1:40400', 'test_agent')
+        notification_service = NotificationService(ignite, '127.0.0.1:40400', 'test_agent1')
         set_connection(cache_service, notification_service)
 
-        print(get_cache_service())
-        print(get_notification_service())
+        agent = await enter_context('test_instance',  lambda: start_agent('test_agent2', True, BoolObject))
+        print(agent)
+        
+        # notification_service.start()
+        # cache_service.stream_create('test_stream', 'test_instance', 'test_agent', 'test_delegate', 2, True, BoolObject)
+        # stream = Stream(PerperStream('test_stream', {}, False, False))
 
-        notification_service.start()
-        cache_service.stream_create('test_stream', 'test_instance', 'test_agent', 'test_delegate', 2, True, BoolObject)
-        stream = Stream(PerperStream('test_stream', {}, False, False))
+        # async def listen_items():
+        #     generator = enter_context('test_instance',  lambda: stream.enumerate())
+        #     async for value in generator:
+        #         print(value)
 
-        async def listen_items():
-            async for value in stream.enumerate():
-                print(value)
+        # await listen_items()
 
-        enter_context('test_instance',  lambda: asyncio.run(listen_items()))
-
-test()
+asyncio.run(test())
