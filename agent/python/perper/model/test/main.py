b1cbc6884ba33c4210fa6ad4a589faa1a3ff5496
Radi Cho <radicho123@gmail.com>
2021-07-02 00:08:58 +0300

    Update sample 1.

--- agent/python/perper/model/test/main.py
+++ agent/python/perper/model/test/main.py
@@ -1,23 +1,25 @@
 import asyncio
-from pyignite.datatypes.primitive_objects import BoolObject
-from perper.model.context import *
-from perper.model.agent import Agent
-from perper.model.stream import Stream
+from pyignite.datatypes.primitive_objects import IntObject
 from pyignite.datatypes.standard import String
+from perper.model.context import *
+from perper.model.agent import *
 from perper.model.bootstrap import initialize
 
-async def generate(params):
-    for x in range(10):
+async def generate(count):
+    for x in range(count):
         await asyncio.sleep(1)
         yield x
 
-async def main(sdgsdg):
-    stream = stream_function('generate', True, BoolObject)
-    print(stream)
+async def process_data(num):
+    return (num + 1, IntObject)
+
+async def main(*args):
+    stream = stream_function('generate', 5, IntObject)
     
     async for value in stream.enumerate():
-        print(value)
-        if value == 9:
+        processed_value = await call_function('process_data', value, IntObject)
+        print(processed_value)
+        if processed_value == 10:
             return ('Done', String)
 
-asyncio.run(initialize('test_agent1', {'test_agent1': main, 'generate': generate}, True))
+asyncio.run(initialize('test_agent1', {'test_agent1': main, 'generate': generate, 'process_data': process_data}, True))
