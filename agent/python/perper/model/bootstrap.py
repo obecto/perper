7e3c6bd008f91d6efb2253f579c7a278c9d13d8b
Radi Cho <radicho123@gmail.com>
2021-07-01 22:00:39 +0300

    Asyncify notification_service.

--- agent/python/perper/model/bootstrap.py
+++ agent/python/perper/model/bootstrap.py
@@ -2,9 +2,11 @@ import sys
 import asyncio
 import random
 from pyignite import Client
+from pyignite.datatypes.primitive_objects import BoolObject
 from perper.model.async_locals import *
 from perper.protocol.cache_service import CacheService
 from perper.protocol.notification_service import NotificationService
+from perper.model.context import *
 
 async def initialize(agent, functions, root=False):
     ignite = Client()
@@ -12,14 +14,21 @@ async def initialize(agent, functions, root=False):
 
     cache_service = CacheService(ignite)
     notification_service = NotificationService(ignite, '127.0.0.1:40400', agent)
-    notification_service.start()
+    await notification_service.start()
     set_connection(cache_service, notification_service)
 
-    if root:
-        await enter_context(f'{agent}-root', functions[agent])
-    else:
-        await listen_triggers(ignite, functions)
+    task1 = asyncio.create_task(listen_triggers(ignite, functions))
+    task2 = asyncio.create_task(enter_context('', lambda: start_agent(agent, None, BoolObject)))
 
+    if root:
+        await task1
+        await task2
+    else:
+        await task1
+
+async def execute_call(functions, call_data, n):
+    (result, result_type) = await enter_context(call_data.instance, lambda: asyncio.create_task(functions[n.delegate](call_data.parameters)))
+    get_cache_service().call_write_result(n.call, result, result_type)
 
 async def listen_triggers(ignite, functions):
     async for (k, n) in get_notification_service().get_notifications(get_local_agent()):
@@ -41,5 +50,4 @@ async def listen_triggers(ignite, functions):
             call_data = calls_cache.get(n.call)
 
             if n.delegate in functions:
-                (result, result_type) = await enter_context(call_data.instance, lambda: asyncio.create_task(functions[n.delegate](call_data.parameters)))
-                get_cache_service().call_write_result(n.call, result, result_type)
+                asyncio.create_task(execute_call(functions, call_data, n))
