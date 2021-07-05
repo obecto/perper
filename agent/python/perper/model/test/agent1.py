8668f66f90d76324fe8f85545ab863d86872979a
Radi Cho <radicho123@gmail.com>
2021-07-05 22:46:40 +0300

    Update the jupyter sample.

--- agent/python/perper/model/test/agent1.py
+++ agent/python/perper/model/test/agent1.py
@@ -4,8 +4,9 @@ from perper.model.context import *
 from perper.model.agent import Agent
 from perper.model.stream import Stream
 from perper.model.bootstrap import initialize
+from pyignite.datatypes.standard import String
 
-async def main():
+async def main(*args):
     (agent2, result) = await start_agent('test_agent2', True, BoolObject)
     print(agent2)
     
@@ -15,9 +16,15 @@ async def main():
     print(agent3)
     
     inc_stream = await agent3.call_function('get_stream', True, BoolObject)
+    csv_contents = ''
     async for v in Stream(inc_stream).enumerate():
         print(v)
-        if v == 9:
-            return
+        if v == ',,':
+            print('Finished!')
+            with open('inc.csv', 'w') as f:
+                f.write(csv_contents)
+            return ('Done', String)
+        
+        csv_contents += v
 
-asyncio.create_task(initialize('test_agent1', {'test_agent1': main}, True))
+asyncio.run(initialize('test_agent1', {'test_agent1': main}, True))
