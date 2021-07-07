99e30bcd830029e91eba6d9cf98800f7332a62c4
Radi Cho <radicho123@gmail.com>
2021-07-07 14:03:01 +0300

    Get addresses and ports with env variables.

--- agent/python/perper/model/bootstrap.py
+++ agent/python/perper/model/bootstrap.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import asyncio
 import random
@@ -10,10 +11,13 @@ from perper.model.context import *
 
 async def initialize(agent, functions, root=False):
     ignite = Client()
-    ignite.connect('127.0.0.1', 10800)
+    ignite_address = os.getenv('PERPER_IGNITE_ADDRESS', '127.0.0.1')
+    ignite_port = int(os.getenv('PERPER_IGNITE_PORT', '10800'))
+    ignite.connect(ignite_address, ignite_port)
 
     cache_service = CacheService(ignite)
-    notification_service = NotificationService(ignite, '127.0.0.1:40400', agent)
+    grpc_address = os.getenv('PERPER_GRPC_ADDRESS', '127.0.0.1:40400')
+    notification_service = NotificationService(ignite, grpc_address, agent)
     await notification_service.start()
     set_connection(cache_service, notification_service)
 
