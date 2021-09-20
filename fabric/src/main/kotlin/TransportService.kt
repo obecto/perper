obecto/perper

commit 9f4f3b82c4830e3977c457bf03431b82816065dd

feat!: Support starting agent instances through docker-compose run

fabric/src/main/kotlin/TransportService.kt
@@ -1,10 +1,13 @@
 package com.obecto.perper.fabric
+import com.obecto.perper.fabric.cache.AgentType
 import com.obecto.perper.fabric.cache.notification.CallResultNotification
+import com.obecto.perper.fabric.cache.notification.CallTriggerNotification
 import com.obecto.perper.fabric.cache.notification.Notification
 import com.obecto.perper.fabric.cache.notification.NotificationKey
 import com.obecto.perper.fabric.cache.notification.NotificationKeyLong
 import com.obecto.perper.fabric.cache.notification.NotificationKeyString
 import com.obecto.perper.fabric.cache.notification.StreamItemNotification
+import com.obecto.perper.fabric.cache.notification.StreamTriggerNotification
 import com.obecto.perper.protobuf.CallNotificationFilter
 import com.obecto.perper.protobuf.FabricGrpcKt
 import com.obecto.perper.protobuf.NotificationFilter
@@ -122,8 +125,15 @@ class TransportService(var port: Int) : Service {
             }
         }.build()
 
-        @kotlinx.coroutines.ExperimentalCoroutinesApi
+        @OptIn(kotlinx.coroutines.ExperimentalCoroutinesApi::class)
         override fun notifications(request: NotificationFilter) = channelFlow<NotificationProto> {
+            val instance = if (request.instance != "") request.instance else null
+            if (instance != null) {
+                InstanceService.setInstanceRunning(ignite, instance, true)
+                InstanceService.setAgentType(ignite, request.agent, AgentType.CONTAINERS)
+            } else {
+                InstanceService.setAgentType(ignite, request.agent, AgentType.FUNCTIONS)
+            }
             val notificationCache = getNotificationCache(ignite, request.agent)
             val notificationAffinity = ignite.affinity<NotificationKey>(notificationCache.name)
             val localNode = ignite.cluster().localNode()
@@ -171,8 +181,10 @@ class TransportService(var port: Int) : Service {
                     }
                     updateQueue(notification.stream)
                 } else if (!confirmed) {
-                    log.trace({ "Sending notification ${request.agent}, $notification - $key" })
-                    runBlocking { send(key.toNotification()) }
+                    if ((notification is CallTriggerNotification && notification.instance == instance) || (notification is StreamTriggerNotification && notification.instance == instance)) {
+                        log.trace({ "Sending notification ${request.agent}, $notification - $key" })
+                        runBlocking { send(key.toNotification()) }
+                    }
                 }
             }
 
@@ -238,7 +250,7 @@ class TransportService(var port: Int) : Service {
                 }
             }
 
-            log.debug({ "Notifications listener started for '${request.agent}'!" })
+            log.debug({ "Notifications listener started for '${request.agent}' - '${request.instance}'!" })
 
             invokeOnClose({ localJob.cancel(); remoteJob.cancel() })
 
@@ -252,7 +264,10 @@ class TransportService(var port: Int) : Service {
                 e.printStackTrace()
                 throw e
             } finally {
-                log.debug({ "Notifications listener finished for '${request.agent}'!" })
+                log.debug({ "Notifications listener finished for '${request.agent}' - '${request.instance}'!" })
+                if (instance != null) {
+                    InstanceService.setInstanceRunning(ignite, instance, false)
+                }
             }
         }.buffer(Channel.UNLIMITED)
 
@@ -311,7 +326,7 @@ class TransportService(var port: Int) : Service {
             }.build()
         }
 
-        @kotlinx.coroutines.FlowPreview
+        @OptIn(kotlinx.coroutines.FlowPreview::class)
         override fun streamIsActive(request: ScaledObjectRef) = flow<Boolean> {
             val notificationCache = getNotificationCache(ignite, request.delegate).withKeepBinary<BinaryObject, BinaryObject>()
 

.editorconfig
@@ -1041,7 +1041,7 @@ csharp_style_var_when_type_is_apparent = true:error
 
 # Prefer method-like constructs to have a block body
 csharp_style_expression_bodied_methods = true:error
-csharp_style_expression_bodied_constructors = true:error
+csharp_style_expression_bodied_constructors = when_on_single_line:error
 csharp_style_expression_bodied_operators = true:error
 
 # New line preferences

.gitignore
@@ -7,6 +7,7 @@
 **/build
 **/dist
 **/*.egg-info
+.eggs/
 .vscode/*
 
 venv

agent/dotnet/Perper.sln
@@ -14,6 +14,8 @@ Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "Solution Items", "Solution 
 EndProject
 Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Perper", "src\Perper\Perper.csproj", "{4E64E360-3BD5-4B40-A48F-3092DE7CC55A}"
 EndProject
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "SimpleContainerAgent", "src\SimpleContainerAgent\SimpleContainerAgent.csproj", "{8D1E2D83-C79C-417D-9FC5-6943CC7182BA}"
+EndProject
 Global
 	GlobalSection(SolutionConfigurationPlatforms) = preSolution
 		Debug|Any CPU = Debug|Any CPU
@@ -28,6 +30,10 @@ Global
 		{4E64E360-3BD5-4B40-A48F-3092DE7CC55A}.Debug|Any CPU.Build.0 = Debug|Any CPU
 		{4E64E360-3BD5-4B40-A48F-3092DE7CC55A}.Release|Any CPU.ActiveCfg = Release|Any CPU
 		{4E64E360-3BD5-4B40-A48F-3092DE7CC55A}.Release|Any CPU.Build.0 = Release|Any CPU
+		{8D1E2D83-C79C-417D-9FC5-6943CC7182BA}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{8D1E2D83-C79C-417D-9FC5-6943CC7182BA}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{8D1E2D83-C79C-417D-9FC5-6943CC7182BA}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{8D1E2D83-C79C-417D-9FC5-6943CC7182BA}.Release|Any CPU.Build.0 = Release|Any CPU
 	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE
@@ -35,6 +41,7 @@ Global
 	GlobalSection(NestedProjects) = preSolution
 		{370B6DC5-2102-4490-8442-3AA20A422AE2} = {384E1F8B-CA65-42C0-83B4-1E7D002A3A14}
 		{4E64E360-3BD5-4B40-A48F-3092DE7CC55A} = {384E1F8B-CA65-42C0-83B4-1E7D002A3A14}
+		{8D1E2D83-C79C-417D-9FC5-6943CC7182BA} = {384E1F8B-CA65-42C0-83B4-1E7D002A3A14}
 	EndGlobalSection
 	GlobalSection(ExtensibilityGlobals) = postSolution
 		SolutionGuid = {7E949E8B-0EC2-4DFC-B593-ECD9454D5579}

agent/dotnet/src/Perper/Application/PerperStartup.cs
@@ -78,9 +78,9 @@ namespace Perper.Application
         #endregion DiscoverStreamAndCallTypes
         #region Services
 
-        public static async Task EnterServicesContext(string agent, Func<Task> context)
+        public static async Task EnterServicesContext(string agent, Func<Task> context, bool useInstance = false)
         {
-            var (cacheService, notificationService) = await InitializeServices(agent).ConfigureAwait(false);
+            var (cacheService, notificationService) = await InitializeServices(agent, useInstance).ConfigureAwait(false);
 
             AsyncLocals.SetConnection(cacheService, notificationService);
 
@@ -90,13 +90,19 @@ namespace Perper.Application
             notificationService.Dispose();
         }
 
-        public static async Task<(CacheService, NotificationService)> InitializeServices(string agent)
+        public static async Task<(CacheService, NotificationService)> InitializeServices(string agent, bool useInstance = false)
         {
             var apacheIgniteEndpoint = Environment.GetEnvironmentVariable("APACHE_IGNITE_ENDPOINT") ?? "127.0.0.1:10800";
             var fabricGrpcAddress = Environment.GetEnvironmentVariable("PERPER_FABRIC_ENDPOINT") ?? "http://127.0.0.1:40400";
+            string? instance = null;
 
             Console.WriteLine($"APACHE_IGNITE_ENDPOINT: {apacheIgniteEndpoint}");
             Console.WriteLine($"PERPER_FABRIC_ENDPOINT: {fabricGrpcAddress}");
+            if (useInstance)
+            {
+                instance = Environment.GetEnvironmentVariable("X_PERPER_INSTANCE") ?? "";
+                Console.WriteLine($"X_PERPER_INSTANCE: {instance}");
+            }
 
             var igniteConfiguration = new IgniteClientConfiguration
             {
@@ -120,7 +126,7 @@ namespace Perper.Application
                 .ExecuteAsync(() => Task.Run(() => Ignition.StartClient(igniteConfiguration))).ConfigureAwait(false);
 
             var cacheService = new CacheService(ignite);
-            var notificationService = new NotificationService(ignite, grpcChannel, agent);
+            var notificationService = new NotificationService(ignite, grpcChannel, agent, instance);
 
             await Policy
                 .Handle<Grpc.Core.RpcException>(ex => ex.Status.DebugException is System.Net.Http.HttpRequestException)
@@ -179,17 +185,13 @@ namespace Perper.Application
 
             taskCollection.Add(async () =>
                 {
-                    await foreach (var (key, notification) in AsyncLocals.NotificationService.GetNotifications(Context.StartupFunctionName).WithCancellation(cancellationToken))
+                    await foreach (var (key, notification) in AsyncLocals.NotificationService.GetCallTriggerNotifications(Context.StartupFunctionName).ReadAllAsync(cancellationToken))
                     {
-                        if (notification is CallTriggerNotification callTriggerNotification)
+                        await AsyncLocals.EnterContext(notification.Instance, notification.Call, async () =>
                         {
-                            var instance = await AsyncLocals.CacheService.GetCallInstance(callTriggerNotification.Call).ConfigureAwait(false);
-                            await AsyncLocals.EnterContext(instance, callTriggerNotification.Call, async () =>
-                            {
-                                await AsyncLocals.CacheService.CallWriteFinished(AsyncLocals.Execution).ConfigureAwait(false);
-                                await AsyncLocals.NotificationService.ConsumeNotification(key).ConfigureAwait(false);
-                            }).ConfigureAwait(false);
-                        }
+                            await AsyncLocals.CacheService.CallWriteFinished(AsyncLocals.Execution).ConfigureAwait(false);
+                            await AsyncLocals.NotificationService.ConsumeNotification(key).ConfigureAwait(false);
+                        }).ConfigureAwait(false);
                     }
                 });
         }
@@ -200,12 +202,9 @@ namespace Perper.Application
             {
                 taskCollection.Add(async () =>
                 {
-                    await foreach (var (key, notification) in AsyncLocals.NotificationService.GetNotifications(callType.Name).WithCancellation(cancellationToken))
+                    await foreach (var (key, notification) in AsyncLocals.NotificationService.GetCallTriggerNotifications(callType.Name).ReadAllAsync(cancellationToken))
                     {
-                        if (notification is CallTriggerNotification callTriggerNotification)
-                        {
-                            taskCollection.Add(ExecuteCall(callType, key, callTriggerNotification));
-                        }
+                        taskCollection.Add(ExecuteCall(callType, key, notification));
                     }
                 });
             }
@@ -217,12 +216,9 @@ namespace Perper.Application
             {
                 taskCollection.Add(async () =>
                 {
-                    await foreach (var (key, notification) in AsyncLocals.NotificationService.GetNotifications(streamType.Name).WithCancellation(cancellationToken))
+                    await foreach (var (key, notification) in AsyncLocals.NotificationService.GetStreamTriggerNotifications(streamType.Name).ReadAllAsync(cancellationToken))
                     {
-                        if (notification is StreamTriggerNotification streamTriggerNotification)
-                        {
-                            taskCollection.Add(ExecuteStream(streamType, key, streamTriggerNotification));
-                        }
+                        taskCollection.Add(ExecuteStream(streamType, key, notification));
                     }
                 });
             }
@@ -233,8 +229,7 @@ namespace Perper.Application
 
         private static async Task ExecuteCall(Type callType, NotificationKey key, CallTriggerNotification notification)
         {
-            var instance = await AsyncLocals.CacheService.GetCallInstance(notification.Call).ConfigureAwait(false);
-            await AsyncLocals.EnterContext(instance, notification.Call, async () =>
+            await AsyncLocals.EnterContext(notification.Instance, notification.Call, async () =>
             {
                 var callInstance = InstanciateType(callType);
 
@@ -247,8 +242,7 @@ namespace Perper.Application
 
         private static async Task ExecuteStream(Type streamType, NotificationKey key, StreamTriggerNotification notification)
         {
-            var instance = await AsyncLocals.CacheService.GetStreamInstance(notification.Stream).ConfigureAwait(false);
-            await AsyncLocals.EnterContext(instance, notification.Stream, async () =>
+            await AsyncLocals.EnterContext(notification.Instance, notification.Stream, async () =>
             {
                 var streamInstance = InstanciateType(streamType);
 
@@ -309,7 +303,7 @@ namespace Perper.Application
                 {
                     if (!parameters[i].HasDefaultValue)
                     {
-                        throw new ArgumentException($"Not enough arguments passed to {type}.{RunAsyncMethodName}; expected at least {i+1}, got {arguments.Length}");
+                        throw new ArgumentException($"Not enough arguments passed to {type}.{RunAsyncMethodName}; expected at least {i + 1}, got {arguments.Length}");
                     }
                     castArgument = parameters[i].DefaultValue;
                 }

agent/dotnet/src/Perper/Model/Agent.cs
@@ -34,5 +34,10 @@ namespace Perper.Model
 
             await AsyncLocals.CacheService.CallReadResult(call).ConfigureAwait(false);
         }
+
+        public async Task Destroy()
+        {
+            await AsyncLocals.CacheService.InstanceDestroy(RawAgent.Instance).ConfigureAwait(false);
+        }
     }
 }

agent/dotnet/src/Perper/Model/Context.cs
@@ -15,18 +15,22 @@ namespace Perper.Model
 
         public IAgent Agent => new Agent(new PerperAgent(AsyncLocals.Agent, AsyncLocals.Instance));
 
-        public async Task<IAgent> StartAgentAsync(string name, params object[] parameters)
+        public async Task<IAgent> StartAgentAsync(string agent, params object[] parameters)
         {
-            var instance = new Agent(new PerperAgent(name, AsyncLocals.CacheService.GenerateName(name)));
-            await instance.CallActionAsync(StartupFunctionName, parameters).ConfigureAwait(false);
-            return instance;
+            var instance = AsyncLocals.CacheService.GenerateName(agent);
+            await AsyncLocals.CacheService.InstanceCreate(instance, agent).ConfigureAwait(false);
+            var model = new Agent(new PerperAgent(agent, instance));
+            await model.CallActionAsync(StartupFunctionName, parameters).ConfigureAwait(false);
+            return model;
         }
 
-        public async Task<(IAgent, TResult)> StartAgentAsync<TResult>(string name, params object[] parameters)
+        public async Task<(IAgent, TResult)> StartAgentAsync<TResult>(string agent, params object[] parameters)
         {
-            var instance = new Agent(new PerperAgent(name, AsyncLocals.CacheService.GenerateName(name)));
-            var result = await instance.CallFunctionAsync<TResult>(StartupFunctionName, parameters).ConfigureAwait(false);
-            return (instance, result);
+            var instance = AsyncLocals.CacheService.GenerateName(agent);
+            await AsyncLocals.CacheService.InstanceCreate(instance, agent).ConfigureAwait(false);
+            var model = new Agent(new PerperAgent(agent, instance));
+            var result = await model.CallFunctionAsync<TResult>(StartupFunctionName, parameters).ConfigureAwait(false);
+            return (model, result);
         }
 
         public async Task<IStream<TItem>> StreamFunctionAsync<TItem>(string functionName, object[] parameters, StreamFlag flags = StreamFlag.Default, QueryEntity queryEntity = null)

agent/dotnet/src/Perper/Model/IAgent.cs
@@ -7,5 +7,7 @@ namespace Perper.Model
         Task<TResult> CallFunctionAsync<TResult>(string functionName, object[] parameters);
 
         Task CallActionAsync(string actionName, object[] parameters);
+
+        Task Destroy();
     }
 }

commit 9cfa86e5e977701c16af1cac535ed9c5313a1e3e

fix: Fixup notification filtering regression
Originally introduced in 9f4f3b82c4830e3977c457bf03431b82816065dd

fabric/src/main/kotlin/TransportService.kt
@@ -181,7 +181,7 @@ class TransportService(var port: Int) : Service {
                     }
                     updateQueue(notification.stream)
                 } else if (!confirmed) {
-                    if ((notification is CallTriggerNotification && notification.instance == instance) || (notification is StreamTriggerNotification && notification.instance == instance)) {
+                    if (instance == null || (notification is CallTriggerNotification && notification.instance == instance) || (notification is StreamTriggerNotification && notification.instance == instance)) {
                         log.trace({ "Sending notification ${request.agent}, $notification - $key" })
                         runBlocking { send(key.toNotification()) }
                     }