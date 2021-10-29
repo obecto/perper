2fb2360b7b5237a9da748b137d4abc624c294213
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2021-10-29 22:51:58 +0300

    chore: remove old code, move samples

--- functions/python/perper/model/stream_flags.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class StreamFlags(Enum):
-    ephemeral = 1
-    default = ephemeral
-    query = 2
-    none = 0
--- samples/Python.FunctionApp/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-dotnet build && cp -R bin/Debug/netcoreapp3.1/bin . && func start
