707b27878cffda203b2cc117e52c0b7eac2bd06b
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2021-11-15 14:43:17 +0200

    chore: Bump versions to beta 1

--- agent/python/setup.py
+++ agent/python/setup.py
@@ -57,7 +57,7 @@ with open("README.md") as f:
 
 setup(
     name="Perper",
-    version="0.8.0b0",
+    version="0.8.0b1",
     packages=setuptools.find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
