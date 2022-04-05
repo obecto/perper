11721bf161d54bbf53b8fcaa23a494fd89f03520
iovigi <il.nedelchev@gmail.com>
2022-04-05 09:18:07 +0300

    Feat: Perper collections (#92)
    * Adding perper collection.
    
    * Implementation of collection
    
    * implementing collection methods and refactoring
    
    * DecrementCount on Pop and Dequeue
    
    * feat: adding query
    
    * refactoring perper collection, split to dictionary and list
    
    * Adding agent instance as field
    
    * Update agent/dotnet/src/Perper/Extensions/Collections/PerperDictionary.cs
    
    Co-authored-by: Bojidar Marinov <bojidar.marinov.bg@gmail.com>
    
    * Update agent/dotnet/src/Perper/Extensions/Collections/PerperDictionary.cs
    
    Co-authored-by: Bojidar Marinov <bojidar.marinov.bg@gmail.com>
    
    * Update agent/dotnet/src/Perper/Extensions/Collections/PerperDictionary.cs
    
    Co-authored-by: Bojidar Marinov <bojidar.marinov.bg@gmail.com>
    
    * Update agent/dotnet/src/Perper/Extensions/Collections/PerperDictionary.cs
    
    Co-authored-by: Bojidar Marinov <bojidar.marinov.bg@gmail.com>
    
    * fixing comments
    
    * using try get value
    
    * fixing GetEnumerator recursion
    
    Co-authored-by: Bojidar Marinov <bojidar.marinov.bg@gmail.com>

---
+++ agent/dotnet/src/Perper/Protocol/FabricService.Collection.cs
@@ -0,0 +1,29 @@
+using Apache.Ignite.Core.Cache.Configuration;
+using Apache.Ignite.Core.Client.Cache;
+
+namespace Perper.Protocol
+{
+    public partial class FabricService
+    {
+        public ICacheClient<int, T> GetListCache<T>(string instance, string name)
+        {
+            var queryEntity = new QueryEntity(typeof(int), typeof(T));
+
+            return Ignite.GetOrCreateCache<int, T>(new CacheClientConfiguration( $"{instance}-list-{name}", queryEntity));
+        }
+
+        public ICacheClient<string, T> GetListMetaCache<T>(string instance, string name)
+        {
+            var queryEntity = new QueryEntity(typeof(string), typeof(T));
+
+            return Ignite.GetOrCreateCache<string, T>(new CacheClientConfiguration($"{instance}-list-{name}-meta", queryEntity));
+        }
+
+        public ICacheClient<TKey, TValue> GetDictionaryCache<TKey, TValue>(string instance, string name)
+        {
+            var queryEntity = new QueryEntity(typeof(TKey), typeof(TValue));
+
+            return Ignite.GetOrCreateCache<TKey, TValue>(new CacheClientConfiguration($"{instance}-dictionary-{name}", queryEntity));
+        }
+    }
+}
