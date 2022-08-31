bdd0123a1cf2d5f4020b035267e97f3fbac62b12
Nikola Staykov <nikola_staykov@abv.bg>
2022-08-31 14:45:30 +0300

    fix: updated paths of dlls to reflect dotnet 6 transition

--- agent/pythonnet/perpernet/bindings/pythonnet.py
+++ agent/pythonnet/perpernet/bindings/pythonnet.py
@@ -4,9 +4,9 @@ import sys
 import os.path
 
 _root_dir = os.path.dirname(os.path.abspath(__file__))
-_perper_utils_folder = os.path.abspath(os.path.join(_root_dir, "../../DotnetUtils/PerperUtils/bin/Debug/net5.0/"))
+_perper_utils_folder = os.path.abspath(os.path.join(_root_dir, "../../DotnetUtils/PerperUtils/bin/Debug/net6.0/"))
 sys.path.append(_perper_utils_folder)
-_dotnet_agent_dll_folder = os.path.abspath(os.path.join(_root_dir, "../../DotnetUtils/DllDump/bin/Debug/net5.0/"))
+_dotnet_agent_dll_folder = os.path.abspath(os.path.join(_root_dir, "../../DotnetUtils/DllDump/bin/Debug/net6.0/"))
 sys.path.append(_dotnet_agent_dll_folder)
 _runtime_path = os.path.abspath(os.path.join(_dotnet_agent_dll_folder, "DllDump.runtimeconfig.json"))
 _runtime = get_coreclr(_runtime_path)
